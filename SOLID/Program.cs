9a46ffa62f525d3dd38753ba4859e753db8db6ee
david.escue@gmail.com <david.escue@gmail.com>
2026-02-27 09:03:53 -0800

    Start SOLID principals project and boilerplate for SingleResponsibility class

---
+++ SOLID/Program.cs
@@ -0,0 +1,18 @@
+﻿using SOLID.SingleResponsibility;
+
+namespace SOLID
+{
+    class Program
+    {
+        static void SingleResponsibility()
+        {
+            var j = new Journal();
+            j.Test();
+        }
+
+        static void Main(string[] args)
+        {
+            SingleResponsibility();
+        }
+    }
+}
---
+++ SOLID/SingleResponsibility/Journal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID.SingleResponsibility
+{
+    internal class Journal
+    {
+        public void Test()
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
